5c952cab929e50a2b7db27a08a752ad05aea3891
samuel <rebuuula@gmail.com>
2024-01-19 13:09:57 -0300

    estrutura base do site criada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
 
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-window">
+      <div className="sidebar">
+
+        <header>
+          <img className="header--avatar" src="https://i.pinimg.com/originals/59/15/e8/5915e871e9a464dcac6bcf331a29d174.jpg" alt="" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}}/>
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}}/>
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}}/>
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          <div className="search--input">
+            
+          </div>
+        </div>
+
+        <div className="chatlist">
+          ...
+        </div>
+        
+      </div>
+      <div className="contentarea">
+        ...
+      </div>
     </div>
   );
 }
-
-export default App;
